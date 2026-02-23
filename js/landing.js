55147365559dec51683230b668e3cc752d9610a4
Marwan-shahata <116221357+Marwan-shahata@users.noreply.github.com>
2026-02-23 13:09:53 +0200

    Update landing.js

--- js/landing.js
+++ js/landing.js
@@ -1,7 +1,5 @@
-window.onload = function () {
-  history.pushState(null, null, location.href);
 
-  window.addEventListener("popstate", function () {
-    history.pushState(null, null, location.href);
-  });
-};
+history.pushState(null, null, location.href);
+window.onpopstate = function () { history.go(1); };
+
+
